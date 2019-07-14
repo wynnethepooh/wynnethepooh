9ec8db9b069a8a842a348d4d4a404966b0c4057b
wynnethepooh <wynnetran@gmail.com>
2019-07-14 00:07:39 -0700

    Add contact form

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -6,6 +6,23 @@ class Contact extends React.Component {
     return (
         <div className="page">
             <h1>contact</h1>
+            <form method="POST">
+              <div className="contact-form">
+                <label htmlFor="name">name</label>
+                <input className="contact-field" type="text" name="name" />
+              </div>
+              <div className="contact-form">
+                <label htmlFor="email">email</label>
+                <input className="contact-field" type="email" name="email" />
+              </div>
+              <div className="contact-form">
+                <label htmlFor="message">message</label>
+                <textarea className="contact-field" name="message" rows="7"></textarea>
+              </div>
+              <div className="contact-form">
+                <input className="submit-button" type="submit" />
+              </div>
+            </form>
         </div>
     );
   }
