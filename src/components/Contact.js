a9a754e0fbcafdd6cbbe649fb79c2a414bcc2aaf
wynnethepooh <wynnetran@gmail.com>
2020-05-17 23:24:02 -0700

    Change color scheme and update default paragraph font

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -4,26 +4,13 @@ import '../App.css';
 class Contact extends React.Component {
   render() {
     return (
+      <>
         <div className="page">
-            <h1>contact</h1>
-            <form method="POST">
-              <div className="contact-form">
-                <label htmlFor="name">name</label>
-                <input className="contact-field" type="text" name="name" />
-              </div>
-              <div className="contact-form">
-                <label htmlFor="email">email</label>
-                <input className="contact-field" type="email" name="email" />
-              </div>
-              <div className="contact-form">
-                <label htmlFor="message">message</label>
-                <textarea className="contact-field" name="message" rows="7"></textarea>
-              </div>
-              <div className="contact-form">
-                <input className="submit-button" type="submit" />
-              </div>
-            </form>
+          <p>
+          If you're interested in my pottery or just want to say hi, send me an email at <a href="mailto:wynnetran@gmail.com">wynnetran@gmail.com</a>.
+          </p>
         </div>
+      </>
     );
   }
 }
