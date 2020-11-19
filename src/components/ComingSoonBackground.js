7445eae930dcff25c4018152bb2bfaa9e8a04a7c
wynnetran <wynnetran@gmail.com>
2020-11-19 11:58:48 -0800

    Update coming soon page

--- src/components/ComingSoonBackground.js
+++ src/components/ComingSoonBackground.js
@@ -1,16 +1,15 @@
 import React from 'react';
 import styled from 'styled-components';
 
-import ComingSoonImage from '../images/terracotta-hanging-planters.jpeg';
+import WavyTerracotta from '../images/wavy-terracotta.png';
 
 const ComingSoonBackground = () => {
 
   return (
     <ComingSoonContainer>
-      <Img src={ComingSoonImage} />
-      <BigCircle />
       <SmallCircle />
-      <Text>coming soon...</Text>
+      <BackgroundImg src={WavyTerracotta} />
+      <ComingSoonText>first collection drops<br /> 11.21.20 at 10am PST</ComingSoonText>
     </ComingSoonContainer>
   );
 };
@@ -26,69 +25,59 @@ const ComingSoonContainer = styled.div`
   }
 `;
 
-const BigCircle = styled.div`
-  width: 100vw;
-  min-width: 100vh;
-  height: 100vw;
-  min-height: 100vh;
-  background: #CD7F5D;
-  border-radius: 50%;
-  position: fixed;
-  z-index: 0;
-  left: 30vw;
-  top: 30vh;
-`;
-
 const SmallCircle = styled.div`
   min-width: 80vh;
   width: 80vw;
   min-height: 80vh;
   height: 80vw;
-  background: #CC8E20;
+  background: #E8C1B7;
   border-radius: 50%;
   position: fixed;
   z-index: 0;
-  right: 30vw;
-  top: 60vh;
-`;
-
-const Img = styled.img`
-  height: 60vh;
-  z-index: 1;
-  position: fixed;
-  vertical-align: middle;
-  margin: auto;
-  top: 0;
-  bottom: 0;
-  right: 0;
-  left: 0;
+  right: 40vw;
+  bottom: 75vh;
 
   @media(max-width: 550px) {
-    height: 70vh;
-    max-width: 70vw;
-    height: auto;
+    left: -70vw;
   }
 `;
 
-const Text = styled.h1`
+const BackgroundImg = styled.img`
+  height: 60vh;
+  z-index: 1;
+  position: fixed;
+  bottom: 0;
+  right: 0;
+
+  @media(max-width: 550px) {
+    height: 50vh;
+  }
+`;
+
+const ComingSoonText = styled.div`
   font-family: 'Jost', sans-serif;
   font-weight: 500;
   font-size: 3rem;
-  letter-spacing: 3px;
-  color: white;
+  letter-spacing: 1px;
+  color: #CC8E20;
   z-index: 3;
+
   position: fixed;
-  bottom: 10vh;
-  right: 10vw;
+  width: 100%;
+  margin: auto;
+  text-align: center;
+  left: 0;
+  top: 40%;
+  height: 70vh;
+  display: inline-block;
+  vertical-align: middle;
+  line-height: normal;
 
   @media (max-width: 700px) {
     font-size: 1.5rem;
-    white-space: nowrap;
-    bottom: 17vh;
   }
 
   @media (max-width: 320px) {
     font-size: 1.2rem;
-    white-space: nowrap;
   }
 `;
